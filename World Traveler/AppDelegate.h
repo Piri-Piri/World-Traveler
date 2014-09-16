556981f2962d03aa3a9f2e3e4a1bc44734d51649
Piri-Piri <david.pirih@gmx.de>
2014-09-17 00:47:11 +0200

    Initial Commit

---
+++ World Traveler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  World Traveler
+//
+//  Created by David Pirih on 17.09.14.
+//  Copyright (c) 2014 Piri-Piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ World Traveler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  World Traveler
+//
+//  Created by David Pirih on 17.09.14.
+//  Copyright (c) 2014 Piri-Piri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
